5e1560e0bcc9347a405683a118daa1220cecf75b
FoxyTony <mirantony@icloud.com>
2021-01-29 15:39:06 +0100

    Saved with failures on styling

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,6 @@ const App = () => {
     <Footer/>
     </>
   )
-}
+};
 
 export default App
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Menu, Segment } from "semantic-ui-react";
 
 const Header = () => {
     return (
-        <nav>
-            <hi id="header">My Portfolio</hi>
-        </nav>
+        <Segment inverted>
+        <Menu inverted secondary>
+            <Menu.item
+            id="header"
+            name="my portfolio"
+            />
+        </Menu>
+    </Segment>    
     );
 };
 

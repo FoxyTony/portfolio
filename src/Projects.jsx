d2e0a296787b58a5fa3742d5e0b947f82860bc30
FoxyTony <mirantony@icloud.com>
2021-01-31 21:41:02 +0100

    Added project folder and refactored it

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import axios from "axios";
 
-const Projects = () => {
-    return (
-        <Container>
-            <h1 id="projects-header">My Projects</h1>
-            <p>
-                Here can you see some of my projects!
-            </p>
-        </Container>
-    );
-};
+class Projects extends Components {
+    state = {
+        projects: [],
+    };
+
+    componentDidMount() {
+        axios.get("./data/projects.json").then((response) => {
+            this.setState({ projects: response.data });
+        });
+    }
+
+    render() {
+        const { projects } = this.state;
+
+        let projectsList = projects.map((project) => {
+            return (
+                <div id={`project-${project.id}`} key={project.id}>
+                    <h3>{project.name}</h3>
+                </div>
+            );
+        });
+
+        return (
+            <Container>
+                <h1 id="projects-header">My Projects</h1>
+                {projectsList}
+            </Container>
+        );
+    }
+}
 
 export default Projects;
